d7b220f10aea6b1060c6b074361a2c97269fe14d
Forte77 <jfortechez@gmail.com>
2026-02-09 16:05:57 -0600

    working on mouse press stuff

--- Shooting Gallery Game/main.lua
+++ Shooting Gallery Game/main.lua
@@ -5,10 +5,19 @@ function love.load()
     target.radius = 50
     score = 0
     timer = 0
+    fSize = love.graphics.newFont(40)
 end
 function love.update(dt)
+    
 end
 function love.draw()
     love.graphics.setColor(1,0,0)
     love.graphics.circle("fill",target.x,target.y,target.radius)
+    love.graphics.setColor(1,215/255,0)
+    love.graphics.setFont(fSize)
+    love.graphics.print(score,0,0)
+end
+function love.mousepressed(x,y,button,istouch,presses)
+    if button == 1 then
+        score = score+1
 end
